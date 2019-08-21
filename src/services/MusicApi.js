85bc03039b679675f745760378147835da3ee0a0
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-21 15:01:14 -0700

    fixed api calls

--- src/services/MusicApi.js
+++ src/services/MusicApi.js
@@ -1,70 +1,44 @@
-export const GetArtists = artists => {
-  return fetch('http://musicbrainz.org/ws/2/artist?query=<SEARCH>&fmt=json&limit=25', {
-    method: 'GET',
-    body: JSON.stringify({ artists: artists }),
-    Header: {
-      'Content-Type': 'application/json'
-    }
-  })
+export const getArtists = (artist = 'nirvana', count = 25) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=${count}`)
+    .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Could not get artists';
-      return console.log('Success!', json);
+      return json;
     });
 };
 
-export const GetArtist = artist => {
-  return fetch('http://musicbrainz.org/ws/2/release?artist=<ARTIST_ID>&fmt=json', {
-    method: 'GET',
-    body: JSON.stringify({ artists: artist }),
-    Header: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(([ok, json]) => {
-      if(!ok) throw 'Could not get artist';
-      return console.log('Success!', json);
-    });
-};
+// export const getArtist = (artist) => {
+//   return fetch(`http://musicbrainz.org/ws/2/release?artist=<ARTIST_ID>&fmt=json`)
+//     .then(res => ([res.ok, res.json()]))
+//     .then(([ok, json]) => {
+//       if(!ok) throw 'Could not get artist';
+//       return console.log('Success!', json);
+//     });
+// };
 
-export const GetCoverArt = coverArt => {
-  return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front', {
-    method: 'GET',
-    body: JSON.stringify({ release: coverArt }),
-    Header: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(([ok, json]) => {
-      if(!ok) throw 'Could not get cover art';
-      return console.log('Success!', json);
-    });
-};
+// export const getCoverArt = (coverArt) => {
+//   return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front')
+//     .then(res => ([res.ok, res.json()]))
+//     .then(([ok, json]) => {
+//       if(!ok) throw 'Could not get cover art';
+//       return console.log('Success!', json);
+//     });
+// };
 
-export const Getlyrics = lyrics => {
-  return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front', {
-    method: 'GET',
-    body: JSON.stringify({ song: lyrics }),
-    Header: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(([ok, json]) => {
-      if(!ok) throw 'Could not get song lyrcis';
-      return console.log('Success!', json);
-    });
-};
+// export const getlyrics = (lyrics) => {
+//   return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front')
+//     .then(res => ([res.ok, res.json()]))
+//     .then(([ok, json]) => {
+//       if(!ok) throw 'Could not get song lyrcis';
+//       return console.log('Success!', json);
+//     });
+// };
 
-export const GetSongs = songs => {
-  return fetch('http://musicbrainz.org/ws/2/recording?release=<RELEASE_ID>&fmt=json', {
-    method: 'GET',
-    body: JSON.stringify({ songs: songs }),
-    Header: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(([ok, json]) => {
-      if(!ok) throw 'Could not get song lyrcis';
-      return console.log('Success!', json);
-    });
-};
-
+// export const getSongs = (songs) => {
+//   return fetch('http://musicbrainz.org/ws/2/recording?release=<RELEASE_ID>&fmt=json')
+//     .then(res => ([res.ok, res.json()]))
+//     .then(([ok, json]) => {
+//       if(!ok) throw 'Could not get song lyrcis';
+//       return console.log('Success!', json);
+//     });
+// };
