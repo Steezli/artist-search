6ce254316cf75b67170c561855fcaa5c87f9ce45
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-22 14:27:03 -0700

    needed that api eh?

--- src/services/MusicApi.js
+++ src/services/MusicApi.js
@@ -1,27 +1,26 @@
 export const getArtists = (artists) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${artists}&fmt=json&limit=25`)
     .then(res => {
-      if(!res.ok) throw 'Could not get the artists releases.';
-      return console.log('Success!'), res.json();
+      if(!res.ok) throw 'Could not get the artists.';
+      return console.log('Successfully retrieved artists!'), res.json();
     });
 };
 
 export const getReleases = (id) => {
   return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
     .then(res => {
-      if(!res.ok) throw 'Could not get the artists releases.';
-      return console.log('Success!'), res.json();
+      if(!res.ok) throw 'Could not get the artist releases.';
+      return console.log(`Successfully retrieved ${id}'s releases!`), res.json();
     });
 };
 
-// export const getCoverArt = (coverArt) => {
-//   return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front')
-//     .then(res => ([res.ok, res.json()]))
-//     .then(([ok, json]) => {
-//       if(!ok) throw 'Could not get cover art';
-//       return console.log('Success!', json);
-//     });
-// };
+export const getSongs = (id) => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`)
+    .then(res => {
+      if(!res.ok) throw 'Could not get the songs for that release.';
+      return console.log(`Successfully retrieved ${id}'s songs!`), res.json();
+    });
+};
 
 // export const getlyrics = (lyrics) => {
 //   return fetch('http://coverartarchive.org/release/<RELEASE_ID>/front')
@@ -32,11 +31,3 @@ export const getReleases = (id) => {
 //     });
 // };
 
-// export const getSongs = (songs) => {
-//   return fetch('http://musicbrainz.org/ws/2/recording?release=<RELEASE_ID>&fmt=json')
-//     .then(res => ([res.ok, res.json()]))
-//     .then(([ok, json]) => {
-//       if(!ok) throw 'Could not get song lyrcis';
-//       return console.log('Success!', json);
-//     });
-// };
