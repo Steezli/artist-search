d37a5ab56408973132cbce1ad0a94cfa40f8a05b
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-22 14:26:49 -0700

    link a release to it's songslist

--- src/components/releases/Release.js
+++ src/components/releases/Release.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Release({ release }) {
   return (
-    <>
+    <Link to={`/release/${release.id}`}>
       <p>{release.title}</p>
-    </>
+    </Link>
   );
 }
 
