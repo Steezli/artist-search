33a2f14eec143194ae24569fe73de73394f9eec9
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-22 14:27:16 -0700

    added the Route fro SongList

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom';
 import SearchArtists from './containers/SearchArtists';
 import DisplayReleases from './containers/DisplayReleases';
+import DisplaySongs from './containers/DisplaySongs';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/release/:id" component={DisplaySongs} />
         <Route path="/artist/:id" component={DisplayReleases} />
         <Route path="/" component={SearchArtists} />
       </Switch>
