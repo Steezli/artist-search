aa7be6be4aec3a74a82a1eab496ca9ffd4988d08
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-26 08:34:00 -0700

    added pagination

--- src/components/containers/DisplayReleases.js
+++ src/components/containers/DisplayReleases.js
@@ -4,20 +4,44 @@ import { getReleases } from '../../services/MusicApi';
 
 export default class DisplayReleases extends PureComponent {
   state = {
-    releases: []
+    releases: [],
+    releaseCount: 0,
+    offset: 0
   }
 
   componentDidMount() {
     getReleases(this.props.match.params.id)
-      .then(({ releases }) => {
-        this.setState({ releases });
+      .then(({ releases, count }) => {
+        this.setState({ releases, releaseCount: count });
       });
+  }
+
+  fetchReleases = () => {
+    getReleases(this.props.match.params.id, this.state.offset)
+      .then(({ releases, count }) => { 
+        this.setState({ releases, releaseCount: count });
+      });
+  }
+
+  pageForward = () => {
+    this.setState(state => ({ offset: state.offset + 25 }), () => {
+      this.fetchReleases();
+
+    });
+  }
+
+  pageBack = () => {
+    this.setState(state => ({ offset: state.offset - 25 }), () => {
+      this.fetchReleases();
+    });
   }
 
   render() {
     return (
       <>
-        <Releases releases={this.state.releases} />
+        <button onClick={() => this.pageBack()}>←</button>
+        <button onClick={() => this.pageForward()}>→</button>
+        <Releases releases={this.state.releases} artist={this.props.match.params.artist} />
       </>
     );
   }
--- src/components/containers/SearchArtists.js
+++ src/components/containers/SearchArtists.js
@@ -2,21 +2,26 @@ import React, { PureComponent } from 'react';
 import Artists from '../artists/Artists';
 import { getArtists } from '../../services/MusicApi';
 import Input from '../Input';
+// import Pagination from '../Pagination';
+
 
 export default class ArtistList extends PureComponent {
   state = {
     artists: [],
-    artist: ''
+    artist: '',
+    artistCount: 0,
+    offset: 0
   }
+
 
   inputChange = ({ target }) => {
     this.setState({ artist: target.value });
   }
 
   fetchArtists = () => {
-    getArtists(this.state.artist)
-      .then(({ artists }) => { 
-        this.setState({ artists });
+    getArtists(this.state.artist, this.state.offset)
+      .then(({ artists, count }) => { 
+        this.setState({ artists, artistCount: count });
       });
   }
 
@@ -24,12 +29,28 @@ export default class ArtistList extends PureComponent {
     return this.fetchArtists();
   }
 
+  pageForward = () => {
+    this.setState(state => ({ offset: state.offset + 25 }), () => {
+      this.fetchArtists();
+
+    });
+  }
+
+  pageBack = () => {
+    this.setState(state => ({ offset: state.offset - 25 }), () => {
+      this.fetchArtists();
+    });
+  }
+
   render() {
     return (
       <>
         <Input artist={this.state.artist}
           inputChange={this.inputChange}
-          onSearch={this.onSearch} />
+          onSearch={this.onSearch}
+        />
+        <button onClick={() => this.pageBack()}>←</button>
+        <button onClick={() => this.pageForward()}>→</button>
         <Artists artists={this.state.artists} />
       </>
     );
