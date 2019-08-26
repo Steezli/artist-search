61bc5c9ccd6560d98b289a26a84be2b5af255a4e
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-08-26 08:29:39 -0700

    link updated

--- src/components/artists/Artist.js
+++ src/components/artists/Artist.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Artist({ artist }) {
   return (
-    <Link to={`/artist/${artist.id}`}>
+    <Link to={`/artist/${artist.name}/${artist.id}`}>
       <p>{artist.name}</p>
     </Link>
   );
